d797e949b177f3a8e361ee62f37ad907472e8810
anephraim <106369104+anephraim@users.noreply.github.com>
2022-07-15 12:34:16 +0300

    Add files via upload

---
+++ ANN MWANGI.c
@@ -0,0 +1,68 @@
+/*
+zzetech c snack bar
+created by ANNE MWANGI
+on july 14th
+c89
+licence MIT
+*/
+
+#include <stdio.h>01
+#include <stdlib.h>
+
+int main()
+
+{
+  int item_[6] = {1,2,3,4,5,6};
+  int item_2;
+
+
+  printf("Welcome: Ann c snack shop\n\n");
+
+  printf("Todays menu");
+
+    printf("What would you like to order\n");
+  printf("food/KES\n");
+  printf("[1]hot Dog/KES90 \n");
+  printf("[2]Hambuger/KES300\n");
+  printf("[3]French fries/KES150\n");
+  printf("[4]milk shake/KES150\n");
+  printf("[5]coffee/KES70\n");
+  printf("[6]Beef smokie/KES40\n");
+  printf("please item number: ");
+  printf("please item quantity: ");
+  scanf("%d" &a);
+  for(int j=0;j<a;j++)
+  {
+      printf("Enter item code: ");
+      scanf("%d",&item_code2);
+
+     }
+     for(i=0;i,a;i++)
+     {
+     printf("%d",item code2);
+
+     }
+
+   return 0;
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
